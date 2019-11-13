7a02484bd24fe07284e72c491768bb005115e581
patrickstecker <Stecker2000@gmail.com>
2019-11-13 13:57:04 +0100

    Reading out Website with Jsoup and parsing into the single day elements.

--- app/src/main/java/com/tinf18ai2/vorlesungsplan/MainActivity.kt
+++ app/src/main/java/com/tinf18ai2/vorlesungsplan/MainActivity.kt
@@ -4,6 +4,9 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.content_main.*
+import org.jsoup.Jsoup
+import org.jsoup.nodes.Document
 
 class MainActivity : AppCompatActivity() {
 
@@ -11,5 +14,20 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
+        displayPlan()
+    }
+
+    private fun displayPlan(){
+        var site : Document = readWebsite("https://vorlesungsplan.dhbw-mannheim.de/index.php?action=view&gid=3067001&uid=7431001")
+
+        var days = site.getElementsByClass("ui-grid-e").first().allElements
+
+        for(day in days){
+
+        }
+    }
+
+    private fun readWebsite(url:String) :  Document{
+        return Jsoup.connect(url).get()
     }
 }
