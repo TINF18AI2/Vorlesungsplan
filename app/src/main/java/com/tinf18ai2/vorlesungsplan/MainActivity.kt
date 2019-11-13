1eac4a3eff2b0604ca583382e90d94e438173509
patrickstecker <Stecker2000@gmail.com>
2019-11-13 14:20:25 +0100

    Added function displayPlan, which analyses the website and picks out the required information.

--- app/src/main/java/com/tinf18ai2/vorlesungsplan/MainActivity.kt
+++ app/src/main/java/com/tinf18ai2/vorlesungsplan/MainActivity.kt
@@ -17,14 +17,20 @@ class MainActivity : AppCompatActivity() {
         displayPlan()
     }
 
-    private fun displayPlan(){
+    private fun displayPlan() : List<Vorlesungstag>{
+        var week : ArrayList<Vorlesungstag> = ArrayList<Vorlesungstag>()
         var site : Document = readWebsite("https://vorlesungsplan.dhbw-mannheim.de/index.php?action=view&gid=3067001&uid=7431001")
 
         var days = site.getElementsByClass("ui-grid-e").first().allElements
 
         for(day in days){
-
+            var items = ArrayList<VorlesungsplanItem>()
+            for(elem in day.getElementsByClass("ui-li ui-li-static ui-body-b")){
+                items.add(VorlesungsplanItem(elem.getElementsByClass("cal-title").first().text(),elem.getElementsByClass("cal-time").first().text(),elem.getElementsByClass("cal-res").first().text()))
+            }
+            week.add(Vorlesungstag(day.getElementsByClass("ui-li ui-li-divider ui-btn ui-bar-b ui-corner-top ui-btn-up-undefined").first().text(),items))
         }
+        return week
     }
 
     private fun readWebsite(url:String) :  Document{
