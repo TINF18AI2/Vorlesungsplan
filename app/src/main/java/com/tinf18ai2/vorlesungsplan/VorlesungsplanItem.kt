16d82bcc9c78f526d8e97b7e8bd69513eff54484
Felix Wortmann <felixwortmann@protonmail.com>
2019-11-13 14:19:01 +0100

    removed date

--- app/src/main/java/com/tinf18ai2/vorlesungsplan/VorlesungsplanItem.kt
+++ app/src/main/java/com/tinf18ai2/vorlesungsplan/VorlesungsplanItem.kt
@@ -3,7 +3,6 @@ package com.tinf18ai2.vorlesungsplan
 
 class VorlesungsplanItem(
     val title: String,
-    val date: String, //Tag der Vorlesung
     val time: String, //Startzeit (Stunde + Minute)
     val description: String //Raum...
 )
