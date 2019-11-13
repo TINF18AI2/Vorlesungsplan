980d760b222f340362672cf5d28eb318d3ec7f58
patrickstecker <Stecker2000@gmail.com>
2019-11-13 15:00:02 +0100

    Made MainActivity functional

--- app/src/main/java/com/tinf18ai2/vorlesungsplan/MainActivity.kt
+++ app/src/main/java/com/tinf18ai2/vorlesungsplan/MainActivity.kt
@@ -8,10 +8,13 @@ import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.content_main.*
 import org.jsoup.Jsoup
 import org.jsoup.nodes.Document
+import java.util.logging.Logger
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var adapter: VorlesungsplanAdapter
+
+    var log : Logger = Logger.getGlobal()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,69 +22,32 @@ class MainActivity : AppCompatActivity() {
         val linearLayoutManager = LinearLayoutManager(this)
         mainRecyclerView.layoutManager = linearLayoutManager
         setSupportActionBar(toolbar)
-        print("test")
-
         adapter = VorlesungsplanAdapter(items = getData(), context = applicationContext)
         mainRecyclerView.adapter = adapter
-        print("MAINRV" + mainRecyclerView)
+    }
+
+    private fun getData() : List<Vorlesungstag>?{    //reads out the Information from the Website and saves it in the returned Array
+        log.info("\n\ndrin\n\n")
+        val week : ArrayList<Vorlesungstag> = ArrayList<Vorlesungstag>()    //Holds Information about the hole week
+        val site : Document = readWebsite("https://vorlesungsplan.dhbw-mannheim.de/index.php?action=view&gid=3067001&uid=7431001")  //This is the raw source code of the website
+
+
+        log.info("\n\ndrin2\n\n")
+        val days = site.getElementsByClass("ui-grid-e").first().allElements     //Array which holds information about every day in the week
+
+        for(day in days){
+            val items = ArrayList<VorlesungsplanItem>() //Every Vorlesung of the day
+            for(elem in day.getElementsByClass("ui-li ui-li-static ui-body-b")){
+                items.add(VorlesungsplanItem(elem.getElementsByClass("cal-title").first().text(),elem.getElementsByClass("cal-time").first().text(),elem.getElementsByClass("cal-res").first().text()))
+            }
+            week.add(Vorlesungstag(day.getElementsByClass("ui-li ui-li-divider ui-btn ui-bar-b ui-corner-top ui-btn-up-undefined").first().text(),items))
+        }
+        log.info("\n\nInfo: "+week.get(0).items.get(0).title+"\n\n")
+        return week
 
     }
 
-    private fun getData(): List<VorlesungsplanItem> {
-        return listOf(
-            VorlesungsplanItem(
-                "test",
-                time = "Zeit",
-                description = "Test"
-            ), VorlesungsplanItem(
-                "test",
-                time = "Zeit",
-                description = "Test"
-            ), VorlesungsplanItem(
-                "test",
-                time = "Zeit",
-                description = "Test"
-            ), VorlesungsplanItem(
-                "test",
-                time = "Zeit",
-                description = "Test"
-            )
-        )
+    private fun readWebsite(url:String) :  Document{
+        return Jsoup.connect(url).get()
     }
-
-//    private fun displayPlan() {
-//        var week: ArrayList<Vorlesungstag> = ArrayList<Vorlesungstag>()
-//        var site: Document =
-//            readWebsite("https://vorlesungsplan.dhbw-mannheim.de/index.php?action=view&gid=3067001&uid=7431001")
-//
-//        var days = site.getElementsByClass("ui-grid-e").first().allElements
-//
-//        for (day in days) {
-//            var items = ArrayList<VorlesungsplanItem>()
-//            for (elem in day.getElementsByClass("ui-li ui-li-static ui-body-b")) {
-//                items.add(
-//                    VorlesungsplanItem(
-//                        elem.getElementsByClass("cal-title").first().text(),
-//                        elem.getElementsByClass("cal-time").first().text(),
-//                        elem.getElementsByClass("cal-res").first().text()
-//                    )
-//                )
-//            }
-//            week.add(
-//                Vorlesungstag(
-//                    day.getElementsByClass("ui-li ui-li-divider ui-btn ui-bar-b ui-corner-top ui-btn-up-undefined").first().text(),
-//                    items
-//                )
-//            )
-//            for (day in days) {
-//
-//            }
-//            return week
-//        }
-//
-//
-//    }
-//    private fun readWebsite(url: String): Document {
-//        return Jsoup.connect(url).get()
-//    }
 }
