2f5d2234433cd45db8e97419336a10c7cd3df6c8
Felix Wortmann <felixwortmann@protonmail.com>
2019-11-13 20:16:52 +0100

    fixed layout issue

--- app/src/main/java/com/tinf18ai2/vorlesungsplan/MainActivity.kt
+++ app/src/main/java/com/tinf18ai2/vorlesungsplan/MainActivity.kt
@@ -1,21 +1,11 @@
 package com.tinf18ai2.vorlesungsplan
 
 import android.os.Bundle
-import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
-
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.content_main.*
-import org.jsoup.Jsoup
-import org.jsoup.nodes.Document
 import java.util.logging.Logger
-import android.net.Uri
-import android.content.Intent
-import androidx.core.app.ComponentActivity
-import androidx.core.app.ComponentActivity.ExtraData
-import androidx.core.content.ContextCompat.getSystemService
-import android.icu.lang.UCharacter.GraphemeClusterBreak.T
 
 
 class MainActivity : AppCompatActivity() {
