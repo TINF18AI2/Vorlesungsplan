76a675c131c5ee3c734dda2930fcf214384f0917
Felix Wortmann <felixwortmann@protonmail.com>
2019-11-14 11:28:10 +0100

    recycler view layout redone

--- app/src/main/java/com/tinf18ai2/vorlesungsplan/VorlesungsplanAdapter.kt
+++ app/src/main/java/com/tinf18ai2/vorlesungsplan/VorlesungsplanAdapter.kt
@@ -1,10 +1,12 @@
 package com.tinf18ai2.vorlesungsplan
 
 import android.content.Context
+import android.graphics.Color
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.cardview.widget.CardView
 import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.main_recycler_view_item_layout.view.*
 
@@ -31,12 +33,21 @@ class VorlesungsplanAdapter(val items: List<VorlesungsplanItem>, val context: Co
     }
 
     override fun onBindViewHolder(holder: VorlesungsPlanItemHolder, position: Int) {
-        holder.titleTextView.text = items.get(position).title
-        holder.timeTextView.text = items.get(position).time
+        val item = items.get(position)
+        holder.titleTextView.text = item.title
+        holder.timeTextView.text = item.time
+        if (item.time.equals("")) {
+            holder.titleTextView.setTextSize(25.toFloat())
+            holder.titleTextView.setTextColor(Color.GRAY)
+        }
     }
 }
 
-class VorlesungsPlanItemHolder(var titleTextView: TextView, var timeTextView: TextView, v: View) :
+class VorlesungsPlanItemHolder(
+    var titleTextView: TextView,
+    var timeTextView: TextView,
+    v: View
+) :
     RecyclerView.ViewHolder(v)
 
 
